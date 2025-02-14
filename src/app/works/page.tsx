8959cc442baa63ec8ca3ebba4ecdc2c8af01e349
bryan308 <cdrcangulo@gmail.com>
2025-02-14 21:50:47 +0800

    refactor: simplify WorksPage and Card components by removing unused code and improving structure

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,41 +1,26 @@
 "use client"
 
-import { useScroll, motion } from "motion/react"
-import { useRef } from "react"
+import React from "react"
+import { motion } from "motion/react"
 import { WorkList } from "./data/workslist"
 import { TList } from "@/components/shared/interface/card"
-import Card from "@/components/shared/works/card"
+import { Card } from "@/components/shared/works/card"
 
 export default function WorksPage() {
-	const container = useRef(null)
-	const { scrollYProgress } = useScroll({
-		target: container,
-		offset: ["start start", "end end"],
-	})
-
 	return (
 		<motion.div
-			ref={container}
-			className="min-h-screen w-full"
-			initial={{ opacity: 0, filter: "blur(8px)" }}
-			animate={{ opacity: 1, filter: "blur(0)" }}
-			viewport={{ amount: 0.5, once: false }}
-			transition={{
-				delay: 0.5,
-				duration: 0.4,
-				ease: "easeInOut",
-			}}
+			className="min-h-screen w-full grid grid-cols-1 md:grid-cols-2 gap-8"
 		>
 			{WorkList.map((l: TList, index: number) => {
-				const targetScale = 1 - (WorkList.length - index) * 0.05
 				return (
 					<Card
 						key={index}
-						i={index}
-						l={l}
-						progress={scrollYProgress}
-						range={[index * 0.25, 1]}
-						targetScale={targetScale}
+						index={index}
+						src={l.src}
+						title={l.title}
+						text={l.text}
+						github={l.github}
+						href={l.href}
 					/>
 				)
 			})}
