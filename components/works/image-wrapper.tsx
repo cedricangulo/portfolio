e9b724659730e8d01e258def2dec8241ba24920e
bryan308 <cdrcangulo@gmail.com>
2025-03-28 20:05:55 +0800

    refactor: enhance ImageWrapper component with improved motion effects and button functionality

--- components/works/image-wrapper.tsx
+++ components/works/image-wrapper.tsx
@@ -1,82 +1,70 @@
 "use client"
 
 import { useState } from "react"
-import { playfair } from "@/lib/fonts"
 import { ImageZoom } from "fumadocs-ui/components/image-zoom"
-import { Info } from "lucide-react"
 import { motion } from "motion/react"
 import { Button } from "@/components/ui/button"
+import { getMotionProps } from "."
+import { InformationCircleIcon } from "@/public/icons"
 
 interface WorkDetails {
-  id: number
-  title: string
-  paragraphs: string[]
-  fontFamily: { fonts: { className: string }; text: string }[]
-  colorScheme: { content: string; color: string; textColor: number }[]
-  langs?: {
-    langName: string
-    langColor: { bg: string; fill: string }
-    langWidth: number
-  }[]
-  images: { src: string; alt: string }[]
+	id: number
+	title: string
+	paragraphs: string[]
+	fontFamily: { fonts: { className: string }; text: string }[]
+	colorScheme: { content: string; color: string; textColor: number }[]
+	langs?: {
+		langName: string
+		langColor: { bg: string; fill: string }
+		langWidth: number
+	}[]
+	images: { src: string; alt: string }[]
 }
 
 const ImageWrapper = ({ work }: { work?: WorkDetails }) => {
-  const [showAllImages, setShowAllImages] = useState(false)
+	const [showAllImages, setShowAllImages] = useState(false)
 
-  if (!work || !work.images) {
-    return null
-  }
+	if (!work || !work.images) {
+		return null
+	}
 
-  return (
-    <motion.div
-      initial={{ opacity: 0, filter: "blur(4px)" }}
-      animate={{ opacity: 1, filter: "blur(0)" }}
-      viewport={{ amount: 0.5, once: false }}
-      transition={{
-        delay: 1,
-        duration: 0.5,
-        ease: "easeInOut",
-      }}
-      className="image-wrapper h-full"
-    >
-      {(showAllImages ? work.images : work.images.slice(0, 2)).map((image, index) => (
-        <figure
-          key={index}
-          className="my-4 text-center"
-        >
-          <ImageZoom
-            src={image.src}
-            alt={image.alt}
-            className="w-auto h-auto shadow-md"
-            width={570}
-            height={325}
-          />
-          <figcaption className={`${playfair.className} fig-caption text-muted-foreground`}>
-            {image.alt}
-          </figcaption>
-        </figure>
-      ))}
-      {!showAllImages && work.images.length > 2 && (
-        <Button
-          variant="ghost"
-          onClick={() => setShowAllImages(true)}
-          className="view-more-button"
-        >
-          View More
-        </Button>
-      )}
-      {work.id === 8 && (
-        <span className="note">
-          <Info
-            strokeWidth={1.5}
-            className="w-6 h-6 text-yellow-600 dark:text-yellow-200"
-          />
-          The data listed above are merely sample data from the internet.
-        </span>
-      )}
-    </motion.div>
-  )
+	return (
+		<motion.div
+			{...getMotionProps(0.3, 1)}
+			className="flex gap-2 flex-wrap w-full h-full"
+		>
+			{(showAllImages ? work.images : work.images.slice(0, 2)).map((image, index) => (
+				<figure
+					key={index}
+					className="my-4 text-center"
+				>
+					<ImageZoom
+						src={image.src}
+						alt={image.alt}
+						className="w-auto h-auto shadow-md"
+						width={570}
+						height={325}
+					/>
+					<figcaption className="mt-4 text-sm italic text-muted-foreground">{image.alt}</figcaption>
+				</figure>
+			))}
+			{work.images.length > 2 && (
+				<Button
+					variant="ghost"
+					onClick={() => setShowAllImages(!showAllImages)}
+					className="mx-auto mt-4"
+				>
+					{showAllImages ? "View Less" : "View More"}
+				</Button>
+			)}
+			{work.id === 8 && (
+				<span className="mt-12 inline-flex items-center gap-4 text-xs p-4 rounded-lg text-yellow-800 dark:text-yellow-50 border border-yellow-500 dark:border-yellow-600 bg-yellow-200/50 dark:bg-yellow-950">
+					<InformationCircleIcon className="size-6 text-yellow-600 dark:text-yellow-200" />
+					The data listed above are merely sample data from the internet.
+				</span>
+			)}
+		</motion.div>
+	)
 }
 
 export default ImageWrapper
