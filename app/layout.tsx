065d4e7817c14be99971b14149bd3694759b0ca1
bryan308 <cdrcangulo@gmail.com>
2025-07-25 18:35:30 +0800

    feat: add ProgressiveBlur component and integrate it into RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import Providers from "@/components/shared/theme-provider"
 import Footer from "@/components/shared/footer"
 import LenisWrapper from "@/components/shared/lenis-wrapper"
 import { geistsans } from "@/lib/fonts"
+import { ProgressiveBlur } from "@/components/shared/progressive-blur"
+import { cn } from "@/lib/utils"
 
 export const metadata: Metadata = {
 	title: {
@@ -33,17 +35,22 @@ export default function RootLayout({ children }: Props) {
 			lang="en"
 			suppressHydrationWarning
 		>
-			<body className={geistsans.className}>
+			<body className={cn("relative antialiased", geistsans.className)}>
 				<Providers>
 					<Header />
 					<LenisWrapper>
 						<main
 							role="main"
-							className="w-full px-8 justify-center flex flex-col items-center overflow-x"
+							className="w-full px-4 md:px-8 justify-center flex flex-col items-center overflow-x"
 						>
 							{children}
 						</main>
 					</LenisWrapper>
+					<ProgressiveBlur
+						className="sticky bottom-[-1px] w-full h-30 z-20"
+						direction="bottom"
+						blurIntensity={1}
+					/>
 					<Footer />
 				</Providers>
 			</body>
