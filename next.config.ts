b83849f15ccf3515f00324486ce5c5cba5fab1fa
cedricangulo <cdrcangulo@gmail.com>
2026-02-28 20:12:29 +0800

    chore: migrate from bun to pnpm and update project configs

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,34 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-      {
-        protocol: "https",
-        hostname: "sow4xmup6y.ufs.sh",
-        port: "",
-        pathname: "/f/**",
-      },
-    ],
-  },
+	async redirects() {
+		return [
+			{
+				source: "/about",
+				destination: "/#about",
+				permanent: true,
+			},
+			{
+				source: "/works",
+				destination: "/#works",
+				permanent: true,
+			},
+		];
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "avatars.githubusercontent.com",
+			},
+			{
+				protocol: "https",
+				hostname: "sow4xmup6y.ufs.sh",
+				port: "",
+				pathname: "/f/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
